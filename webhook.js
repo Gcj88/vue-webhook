3323746ec60da4ccd059d06defd10f6e63955431
高仓健 <gaocangjian@bosssoft.com.cn>
2021-12-25 19:43:29 +0800

    update

--- webhook.js
+++ webhook.js
@@ -24,7 +24,7 @@ let server = http.createServer(function(req,res){
             res.end(JSON.stringify({ok:true}));
             if(event == 'push'){
                 let payload = JSON.parse(body);
-                let child = spawn('sh',[`./${payload.repositry,name}.sh`]);
+                let child = spawn('sh',[`./${payload.repositry.name}.sh`]);
                 let buffers = [];
                 child.stdout.on('data',function(buffer){
                     buffers.push(buffer);
