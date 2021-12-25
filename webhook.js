34263eb7b3fbd69b973ddfc10d8526e7cc1e2bde
高仓健 <gaocangjian@bosssoft.com.cn>
2021-12-25 18:41:52 +0800

    update

--- webhook.js
+++ webhook.js
@@ -1,5 +1,6 @@
 let http = require('http');
 let crypto = require('crypto');
+let {spawn} = require('child_process');
 let SECRET = '123456';
 function sign(body){
     return 'sha1='+crypto.createHmac('sha1',SECRET).update(body).digest('hex');
@@ -13,15 +14,28 @@ let server = http.createServer(function(req,res){
         });
         req.on('end',function(buffer){
             let body = Buffer.concat(buffer);
-            let event = req.headers['x-gitHub-event'];//event=push
+            let event = req.headers['x-github-event'];//event=push
             //git请求过来时既传递请求体body，又传递签名signature，需要验证签名是否正确
             let signature = req.headers['x-hub-signature'];
             if(signature !== sign(body)){
                 res.end('Not Allowed!');
             }
+            res.setHeader('Content-Type','application/json');
+            res.end(JSON.stringify({ok:true}));
+            if(event == 'push'){
+                let payload = JSON.parse(body);
+                let child = spawn('sh',[`./${payload.repositry,name}.sh`]);
+                let buffers = [];
+                child.stdout.on('data',function(buffer){
+                    buffers.push(buffer);
+                });
+                child.stdout.on('end',function(buffer){
+                    let logs = Buffer.concat(buffers);
+                    console.log(logs);
+                });
+            }
         });
-        res.setHeader('Content-Type','application/json');
-        res.end(JSON.stringify({ok:true}));
+       
     }else{
         res.end('Not Found!');
     }
